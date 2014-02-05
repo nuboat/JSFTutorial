0aea7b4ca48a5fc6552e6faef791a659c81380f3
nuboat <nuboat@gmail.com>
2014-02-06 01:30:47 +0700

    2. Setup Project to run JSF

---
+++ src/main/java/com/thjug/managedbean/IndexManagedBean.java
@@ -0,0 +1,42 @@
+/*
+ * Attribution
+ * CC BY
+ * This license lets others distribute, remix, tweak,
+ * and build upon your work, even commercially,
+ * as long as they credit you for the original creation.
+ * This is the most accommodating of licenses offered.
+ * Recommended for maximum dissemination and use of licensed materials.
+ *
+ * http://creativecommons.org/licenses/by/3.0/
+ * http://creativecommons.org/licenses/by/3.0/legalcode
+ */
+package com.thjug.managedbean;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author nuboat
+ */
+@RequestScoped
+@ManagedBean(name = "index")
+public class IndexManagedBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String hello;
+
+	public IndexManagedBean() {
+		hello = "Hello World !";
+	}
+
+	public String getHello() {
+		return hello;
+	}
+
+	public void setHello(final String hello) {
+		this.hello = hello;
+	}
+
+}
